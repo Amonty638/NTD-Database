f9f3dc83021867385c70b41c62902b48342f0173
wDutton <wdutton@students.stonehill.edu>
2019-05-03 12:41:56 -0400

    Created dao for customer order and changes to customer order

---
+++ database/CustomerOrderDAO.py
@@ -0,0 +1,84 @@
+from database.CustomerOrder import CustomerOrder
+from database.Connect import Connect
+
+class CustomerOrderDAO(Connect):
+    def __init__(self):
+        Connect.__init__(self)
+
+    def select_all(self):
+        connect = Connect()
+        connect.sql_execute('select * from customer_order')
+        customer_order_list = []
+        for row in connect.cur:
+            customer_order = CustomerOrder()
+            customer_order.set_values_from_row(row)
+            customer_order_list.append(customer_order)
+        return customer_order_list
+
+    def select(self, key):
+        connect = Connect()
+        connect.sql_execute('select * from customer_order where hold# = ' + "'" + str(key) + "'")
+        row = connect.cur.fetchone()
+        customer_order = CustomerOrder()
+        customer_order.set_values_from_row(row)
+        return customer_order
+
+    def insert_customer_order(self, customerOrderObject):
+        connect = Connect()
+        print("Insert into  customer_order values (" + customerOrderObject.get_value_string())
+        connect.sql_execute('Insert into customer_order values ( ' + customerOrderObject.get_value_string())
+        connect.commit()
+
+
+
+    def update(self, customerOrderObject):
+        connect = Connect()
+        connect.sql_execute("update customer_order set " +
+                            "date_made = '" + customerOrderObject.get_date_made() + "'," +
+                            "total_cost = " + str(customerOrderObject.get_total_cost()) + "," +
+                            "hold# = '" + customerOrderObject.get_hold_num() + "', " +
+                            "delivery_address = '" + customerOrderObject.get_delivery_address() + "', " +
+                            "phone# = '" + customerOrderObject.get_phone_num() + "' " +
+                            "where hold# = '" + customerOrderObject.get_hold_num() + "'")
+        connect.commit()
+
+    def delete_customer_order(self, hold_num):
+        connect = Connect()
+        connect.sql_execute("delete from customer_order where hold# = " + "'" + hold_num + "'")
+        connect.commit()
+
+
+#TESTING STUFF
+custorder = CustomerOrder()
+custorder_dao = CustomerOrderDAO()
+custorder.set_phone_num("123-123-1234")
+custorder.set_delivery_address("330 Main street north reading ma")
+custorder.set_description("Kitchen")
+custorder.set_hold_num("98765")
+custorder.set_total_cost(0.00)
+custorder.set_date_made("5/3/19")
+
+custorder_dao.insert_customer_order(custorder)
+stuff = custorder_dao.select_all()
+print("Just inserted: ")
+for things in stuff:
+    print(things.get_dictionary())
+
+custorder.set_total_cost(10.00)
+custorder_dao.update(custorder)
+
+print("\n\n After updateing total cost")
+stuff = custorder_dao.select_all()
+print("Just inserted: ")
+for things in stuff:
+    print(things.get_dictionary())
+
+print("\n\n Doing a select ")
+print(custorder_dao.select("98765").get_dictionary())
+
+print("\n\n Doing a delete ")
+custorder_dao.delete_customer_order("98765")
+stuff = custorder_dao.select_all()
+for things in stuff:
+    print(things.get_dictionary())
+
